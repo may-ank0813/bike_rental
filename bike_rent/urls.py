47b1f112827bfdd21ac16e89029bc806ccc3752d
may-ank0813 <mayanksaini1913@gmail.com>
2022-03-06 17:18:04 +0530

    urls.py changes

--- bike_rent/urls.py
+++ bike_rent/urls.py
@@ -20,7 +20,7 @@ from accounts.views import (login_view, register_view, logout_view)
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.static import serve
-from django.conf.urls import url
+from django.urls import  re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -41,7 +41,7 @@ urlpatterns = [
 
     path('<int:id>/deletemsg/', msg_delete, name = "msg_delete"),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
