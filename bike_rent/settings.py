921a9a48a56a32914158c850abd8efe86dd8c848
may-ank0813 <mayanksaini1913@gmail.com>
2022-03-07 00:36:32 +0530

    templates changes

--- bike_rent/settings.py
+++ bike_rent/settings.py
@@ -120,12 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 
 ]
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+
 
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
